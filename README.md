1cf76d251ff71b5c764dfc5ede72564c38d6d30f
Catalina <catalina.genovese.7e8@itb.cat>
2025-05-15 09:42:18 +0200

    commit para marc-chwwaaaan

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# Projecte_Sostenibilitat_Node
+Focalitzat en l'ODS 13 Acció climàtica – Integrants: Cata, Saray, Marc, Leo sdsadas 
---
+++ main.js
@@ -0,0 +1,24 @@
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+
+const app=express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.set('views', './views');
+
+const readData=()=>{
+    try{
+        const data=fs.readFileSync(".JSON/IncendisForestals.json");
+        return JSON.parse(data)
+
+    }catch(error){
+        console.log(error);
+    }
+};
+
+app.get("/", function(req, res) {
+    const data = readData();
+    res.render("Forests",{data})
+});
